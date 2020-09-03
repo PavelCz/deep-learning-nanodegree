e05d301ab39c1bbf54bcbda69171663cdaf149d9
Pavel C <pavelc.git@posteo.net>
2020-09-03 13:29:37 +0200

    Fix transpose

--- project-bikesharing/my_answers.py
+++ project-bikesharing/my_answers.py
@@ -97,7 +97,7 @@ class NeuralNetwork(object):
         hidden_error_term = hidden_outputs * (1 - hidden_outputs) * hidden_error
 
         # Weight step (input to hidden)
-        delta_weights_i_h += hidden_error_term.T * X[:, None]
+        delta_weights_i_h += hidden_error_term * X[:, None]
         # Weight step (hidden to output)
         delta_weights_h_o += output_error_term * hidden_outputs[:, None]
         return delta_weights_i_h, delta_weights_h_o
